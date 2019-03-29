78bb0dbd0b35f020dcd6e132efdfbba6098577db
Casey <caseywhe@buffalo.edu>
2019-03-28 23:53:10 -0400

    electron wrapping for desktop

---
+++ game/main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// window var
+let wind;
+
+// function to create window
+app.on('ready', () => {
+    wind = new BrowserWindow({width: 800, height: 600,
+        icon: path.join(__dirname, 'assets/player.png')});
+
+    // load index.html
+    wind.loadFile("./index.html")
+        /*url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes:true*/
+    ;
+
+
+    // include web contents
+    wind.webContents.openDevTools();
+
+
+    wind.on('closed', ()=>{
+        wind = null
+    });
+
+})
+
+//app.on('ready', creatWindow())
